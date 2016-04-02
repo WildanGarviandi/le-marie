ed7316a25131cf1ade91702840c48030b8d42c54
Wildan <garviandiwildan@gmail.com>
2016-04-02 15:51:01 +0700

    change landing image

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET order listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('order', { title:'Order' });
 });
 
